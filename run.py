20fe2704967402a70f7f7068b0593f14629f61c1
angela3969 <64839887+angela3969@users.noreply.github.com>
2022-03-08 22:46:30 +0800

    Update run.py

--- run.py
+++ run.py
@@ -1,27 +1,9 @@
-#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-"""
-Created on Wed Mar  2 22:44:29 2022
-
-@author: angela
-"""
-
-from flask import Flask
-#import job
-from flask import request
 from app import app
-app = Flask(__name__)
-@app.route("/", methods=['POST'])
-def hello():
-    return "Hello, World!"
 
-@app.route("/job", methods=['POST'])
-def jobSearch():
-    insertValues = request.get_json()
-    x1=insertValues['career']
-    print(x1)
-    job.getJob(x1)
-    return "Done!"
+@app.route("/")
+def index():
+    return "Flask Api Start"
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0",port=3000,debug=False)
