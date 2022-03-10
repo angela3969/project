d6b4adbe6b0b6bfbe329b16b039269b51a4a6fc7
angela3969 <64839887+angela3969@users.noreply.github.com>
2022-03-10 20:19:16 +0800

    Update job.py

--- app/job.py
+++ app/job.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-# -*- coding: utf-8-sig -*-
+# -*- coding: utf-8 -*-
 """
 Created on Wed Mar  2 22:46:25 2022
 
@@ -28,4 +28,77 @@ def getSkill(applyURL):
   content = json.loads(response.text)
   print(content)
   data = (content['data'])['condition']
-  return data
+  dataDict = {}
+  
+  #
+  major = data['major']
+  if(major !=""):
+    print(major)
+    #return major
+    dataDict['major'] = major
+    
+  #
+  languages = data['language']
+  dataDict['language'] = []
+  for language in languages:
+    languageRequired = language['language']
+    abilities = language['ability'].split("、")
+    for ability in abilities:
+      try:
+        abilityRequired = ability.split("/")
+        requiredLanguageAbility = languageRequired+"("+abilityRequired[0]+")"
+        level = abilityRequired[1]
+        dataDict['language'].append([requiredLanguageAbility],level)
+        #saveSkill(line,jobId,requiredLanguageAbility,level,category)  
+      except:
+        print('')
+        
+  #
+  Specialties = data['specialty']
+  dataDict['specialty'] = []
+  for specialty in Specialties:
+    try:
+      toolSpecialty = specialty['description']
+      dataDict['specialty'].append(toolSpecialty )
+    except:
+      print('')
+      
+  #
+  skills = data['skill']
+  dataDict['skill'] = []
+  for skill in skills:
+    try:  
+      skillRequired = skill['description']
+      dataDict['skill'].append(skillRequired)
+    except:
+      print('')
+      
+  #
+  certificates = data['certificate']
+  dataDict['certificate'] = []
+  for certificate in certificates:
+    try:
+      certificateRequired = certificate['description']
+      dataDict['certificate'].append(certificateRequired)
+    except:
+      print('')
+  
+  #
+  driverLicenses = data['driverLicense']
+  dataDict['driverLicense'] = []
+  for driverLicense in driverLicenses:
+    try:
+      driverLicenseRequired = driverLicense['description']
+      dataDict['driverLicense'].append(driverLicenseRequired )
+    except:
+      print('')
+  
+  
+  #
+  others = data['other']
+  try:
+     dataDict['others'] = others
+  except:
+     print('')
+      
+  return dataDict
