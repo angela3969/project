c0d1cb42b4e872cf6528b5ead164d1dbe3eb456c
angela3969 <64839887+angela3969@users.noreply.github.com>
2022-03-10 17:04:53 +0800

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,10 @@
 from flask import Flask
 import job
 from flask import request
+from flask_cors import CORS
 app = Flask(__name__)
+CORS(app)
+
 @app.route("/test", methods=['POST'])
 def hello():
     return "Hello, World!"
@@ -9,7 +12,7 @@ def hello():
 @app.route("/job", methods=['POST'])
 def jobSearch():
     insertValues = request.get_json()
-    x1=insertValues['career']
+    x1=insertValues['url']
     print(x1)
-    job.getJob(x1)
-    return "Done!"
+    job.getSkill(x1)
+    return job.getSkill(x1)
